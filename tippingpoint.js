6ea34c3b93da111958b0913fcbb7c93d18335591
Chris Reimer <chrisreimer5731@gmail.com>
2021-08-06 20:09:18 -0400

    Update tippingpoint.js

--- tippingpoint.js
+++ tippingpoint.js
@@ -1,4 +1,4 @@
-let version="0.1.9b"
+let version="0.1.9c"
 
 let yellow; //Color Presets
 let purple;
@@ -534,8 +534,8 @@ function updateManual(){
   scaledText("Version 1.1",0,-170+27,regular,13);
   if(disableHover){
   if(manualButtons[0].clicked)window.open("https://link.vex.com/docs/21-22/vrc/tipping-point/Game-Manual","_blank");
-  else if(manualButtons[1].clicked)window.open("https://link.vex.com/docs/21-22/vrc/tipping-point/Appendix-A","_blank");
-  else if(manualButtons[2].clicked)window.open("https://link.vex.com/docs/21-22/vrc/tipping-point/Appendix-B","_blank");
+  else if(manualButtons[1].clicked)window.open("https://link.vex.com/docs/21-22/vrc/tipping-point/Appendix-A","_self");
+  else if(manualButtons[2].clicked)window.open("https://link.vex.com/docs/21-22/vrc/tipping-point/Appendix-B");
   else if(manualButtons[3].clicked)window.open("https://link.vex.com/docs/21-22/vrc/tipping-point/Appendix-C","_blank");
   else if(manualButtons[4].clicked)window.open("https://link.vex.com/docs/21-22/vrc/tipping-point/Appendix-D","_blank");
   else if(manualButtons[5].clicked)window.open("https://link.vex.com/docs/21-22/vrc/tipping-point/Appendix-E","_blank");
